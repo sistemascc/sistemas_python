4a693895731ac5df8078faa16cb53b67834c2fa7
sistemasccltm <sistemas@ccuauhtemoc.com>
2020-01-21 12:34:12 -0600

    actualizados

---
+++ estructuras.py
@@ -0,0 +1,10 @@
+precio = 150
+if precio > 100:
+    precio = precio - 20
+elif precio >=150:
+    precio +=50
+else:
+    precio = precio + 20
+print(precio)
+
+
---
+++ variables.py
@@ -0,0 +1,6 @@
+numero = 100
+
+lista_a = [1, "Computer", "Python Class", 5]
+print(numero, " ", lista_a)
+
+
